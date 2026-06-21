fbf243fa60fd2af7c957adb86f4cef23433897a0
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-06-21 18:36:19 +0530

    updation

--- LearningJAVA/pattern.java
+++ LearningJAVA/pattern.java
@@ -94,15 +94,43 @@
 //         }
 //     }
 // }
-class pattern{
-    public static void main(String args[]){
-        for(int i = 1;i<=5;i++){
-            for(int j = 1;j<5;j++){
-                System.out.print(" ");}{
-            
-            for(int j =1;j<=i;j++){
-                System.out.print("*");
-            }System.out.println();
+// class pattern{
+//     public static void main(String args[]){
+//         int n = 5;
+//         for(int i = 1;i<=n;i++){
+//             for(int j =n-i;j>=1;j--){
+//                 System.out.print(" ");
+//             }
+//             for(int j =1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//             }
+//         }
+// }
 
-            }}}
-}
+// Print   1
+//         0 1
+//         1 0 1
+//         0 1 0 1
+//         1 0 1 0 1
+// import java.util.*;
+// class pattern{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for(int i =1 ; i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 if((i+j)%2==0){
+//                     System.out.print("1");
+//                 }
+//                 else{
+//                     System.out.print("0");
+                
+//                 }
+
+//             }System.out.println();
+//         }
+//     } 
+// }
+
