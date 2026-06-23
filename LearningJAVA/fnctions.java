ecfbd7340075f47877f7579303dab25aa5ad36b1
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-06-23 19:25:58 +0530

    updation

--- LearningJAVA/fnctions.java
+++ LearningJAVA/fnctions.java
@@ -1,17 +1,33 @@
+// print my name using function
+// import java.util.*;
+// public class fnctions {
+//     public static void printMyname(String name){
+//         System.out.println(name);
+//         return;
+//     }
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.next();
 
-import java.util.*;
-public class fnctions {
-    public static void printMyname(String name){
-        System.out.println(name);
-        return;
-    }
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-
-        printMyname(name);
+//         printMyname(name);
 
 
-    }
+//     }
     
-}
+// }
+//Q. sum of 2 using function
+// import java.util.*;
+// public class fnctions{
+//     public static int calculateSum(int a,int b){
+//        int sum = a + b ;
+//         return sum; 
+//     }
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//             int a = sc.nextInt();
+//             int b = sc.nextInt();
+//             int sum = calculateSum(a, b);
+//             System.out.println(sum);
+            
+//     }
+// }
