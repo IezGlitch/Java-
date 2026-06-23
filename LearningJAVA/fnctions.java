bda93fb6706d6745aa025962da1cda16ba654a47
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-06-23 23:31:57 +0530

    updation

--- LearningJAVA/fnctions.java
+++ LearningJAVA/fnctions.java
@@ -31,3 +31,48 @@
             
 //     }
 // }
+// Product of 2 numbers
+// import java.util.*;
+// public class fnctions{
+//     public static int calculateProduct(int a, int b){
+//         int product = a * b;
+//         return product;
+//     }
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//             int a = sc.nextInt();
+//             int b = sc.nextInt();
+//             int product = calculateProduct(a, b);
+//             System.out.println("Sum of two numbers are" +" "+ product);
+
+        
+//     }
+// }
+// Q. factorial of a number
+// import java.util.*;
+// public class fnctions{
+//     public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int factorial = 0;
+//     int fact = 1 ;
+//     int n = sc.nextInt();
+//     for (int i = n ;i>=1;i--){
+//         fact = n*n-1;
+//         factorial= factorial + fact;
+// }System.out.println(factorial);
+// }}
+//another approach for same question
+// import java.util.*;
+// public class fnctions{
+//     public static void factorial1(int n){
+//     int fact = 1;
+//     for (int i = n;i>=1;i--){
+//         fact = fact * i;
+//     }System.out.println(fact);
+//     return;}
+//     public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     factorial1(n);
+// }
+// }
