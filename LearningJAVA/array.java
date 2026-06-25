ef7e9d1ab2d0bfb555db63b76a2416b4b9e20ac0
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-06-25 23:36:35 +0530

    updation

--- LearningJAVA/array.java
+++ LearningJAVA/array.java
@@ -42,3 +42,18 @@
 //     }
 
 // }
+import java.util.*;
+public class array{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int numbers[] = new int[n];
+        for (int i = 0;i<n;i++){
+            numbers[i]=sc.nextInt();}
+        for (int i =0;i<n;i++){
+            if(i==20){
+                System.out.println(i);
+            }
+        }
+        }
+    }
