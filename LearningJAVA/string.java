e475fb558fb2a7bcb613b7d01679af3fb7ded5fb
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-06-27 02:10:18 +0530

    String

---
+++ LearningJAVA/string.java
@@ -0,0 +1,11 @@
+//Contactination
+import java.util.*;
+public class string{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String firstName = sc.next();
+        String lastName = sc.next();
+        String fullName = firstName +" "+ lastName;
+        System.out.println(fullName);
+    }
+}
