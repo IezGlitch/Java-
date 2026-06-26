1bb994eb7a0189dac74afae06541a70105f1c56f
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-06-27 00:00:04 +0530

    update

--- LearningJAVA/twodarray.java
+++ LearningJAVA/twodarray.java
@@ -1,20 +1,45 @@
+// import java.util.*;
+// public class twodarray {
+//     public static void main(String args[] ){
+//         Scanner sc = new Scanner(System.in);
+//         int rows = sc.nextInt();
+//         int cols = sc.nextInt();
+//         int matrix[][]=new int [rows][cols];
+//         for(int i = 0;i<rows;i++){
+//             for(int j = 0;j<cols;j++){
+//                 matrix[i][j]=sc.nextInt();
+//             }
+//         }
+//         for(int i =0;i<rows;i++){
+//             for(int j =0;j<cols;j++){
+//                 System.out.print(matrix[i][j]+" ");
+//             }System.out.println();
+//         }
+//     }
+    
+// }
 import java.util.*;
-public class twodarray {
-    public static void main(String args[] ){
+public class twodarray{
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int rows = sc.nextInt();
+        System.out.println("Enter the no. of rows you wanted in your matrix");
+        int rows= sc.nextInt();
+        System.out.println("Enter the numbers of clos you want in your matrix");
         int cols = sc.nextInt();
-        int matrix[][]=new int [rows][cols];
+        int matrix[][] = new int [rows][cols];
+        System.out.println("Enter the num of whose you want to search the index");
+        int x = sc.nextInt();
         for(int i = 0;i<rows;i++){
-            for(int j = 0;j<cols;j++){
+            for(int j= 0;j<cols;j++){
                 matrix[i][j]=sc.nextInt();
+             }System.out.println();
+        }
+        for(int i = 0;i<rows;i++){
+            for(int j =0;j<cols;j++){
+                if(matrix[i][j]==x){
+                    System.out.println("x found at location" + i+","+j);
+                }
             }
         }
-        for(int i =0;i<rows;i++){
-            for(int j =0;j<cols;j++){
-                System.out.print(matrix[i][j]+" ");
-            }System.out.println();
-        }
     }
-    
 }
