5179ccf8f43c359949514e7170d27a4abc8a5392
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-06-27 02:21:38 +0530

    update

--- LearningJAVA/string.java
+++ LearningJAVA/string.java
@@ -1,11 +1,26 @@
 //Contactination
-import java.util.*;
-public class string{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        String firstName = sc.next();
-        String lastName = sc.next();
-        String fullName = firstName +" "+ lastName;
-        System.out.println(fullName);
-    }
-}
+// import java.util.*;
+// public class string{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         String firstName = sc.next();
+//         String lastName = sc.next();
+//         String fullName = firstName +" "+ lastName;
+//         System.out.println(fullName);
+//     }
+// }  
+
+//String length and String charAT()
+// import java.util.*;
+//     public class string{
+//         public static void main(String args[]){
+//             Scanner sc = new Scanner(System.in);
+//             System.out.println("Enter the name");
+//             String name = sc.next();
+//             for(int i = 0;i<name.length();i++){
+//                 System.out.println(name.charAt(i));
+//             }
+
+//         }
+    
+// } 
