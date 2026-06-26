7f377166b154529f4fd90a51b8c00c8b8e40a37b
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-06-27 01:17:42 +0530

    update

--- LearningJAVA/array.java
+++ LearningJAVA/array.java
@@ -42,20 +42,41 @@
 //     }
 
 // }
-import java.util.*;
-public class array{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Size of your Array");
-        int n = sc.nextInt();
-        int numbers[] = new int[n];
-        System.out.println("Enter the number of whom you want to find its index");
-        int x = sc.nextInt();
-        for (int i = 0;i<n;i++){
-            numbers[i]=sc.nextInt();}
-        for (int i =0;i<numbers.length;i++){
-            if(numbers[i]==x) {
-           System.out.println("Index of that given number is "+i);
-        } }
-        }
-}
+//q.Create a array take input from user search a indices of a provided number
+// import java.util.*;
+// public class array{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the Size of your Array");
+//         int n = sc.nextInt();
+//         int numbers[] = new int[n];
+//         System.out.println("Enter the number of whom you want to find its index");
+//         int x = sc.nextInt();
+//         for (int i = 0;i<n;i++){
+//             numbers[i]=sc.nextInt();}
+//         for (int i =0;i<numbers.length;i++){
+//             if(numbers[i]==x) {
+//            System.out.println("Index of that given number is "+i);
+//         } }
+//         }
+// }
+
+
+// Q.Take names as input create an array and print it
+
+// import java.util.*;
+// public class array{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter The size of the array");
+//         int size = sc.nextInt();
+//         String Names[] = new String[size];
+//         for(int i = 0;i<size;i++){
+//             Names[i] = sc.next();
+//         }
+//         for(int i = 0;i<size;i++){
+//             System.out.print(Names[i]+",");
+//         }
+
+//     }
+// }
