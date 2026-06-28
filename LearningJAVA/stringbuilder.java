7a535100542c4150d0081a33fe4c50e131a874cc
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-06-28 23:34:13 +0530

    updation

--- LearningJAVA/stringbuilder.java
+++ LearningJAVA/stringbuilder.java
@@ -1,3 +1,10 @@
+//charAt of String builder
 public class stringbuilder {
-    
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("Happy");
+        System.out.println(sb);
+        System.out.println(sb.charAt(0));
+        sb.setCharAt(0,'P');
+        System.out.println(sb);
+    }
 }
