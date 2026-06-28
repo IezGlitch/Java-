fc6f1c5f269cf6660ff4f69aeba8c9317183c932
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-06-28 23:57:11 +0530

    updation

--- LearningJAVA/stringbuilder.java
+++ LearningJAVA/stringbuilder.java
@@ -2,12 +2,19 @@
 public class stringbuilder {
     public static void main(String args[]){
         StringBuilder sb = new StringBuilder("Happy");
-        // System.out.println(sb);
-        // System.out.println(sb.charAt(0));
-        // sb.setCharAt(0,'P');
-        // System.out.println(sb);
-        // sb.insert(0,'S');
-        sb.insert(2,'e');
         System.out.println(sb);
+        // System.out.println(sb.charAt(0)); //Print index 0
+        // sb.setCharAt(0,'P');      change in character
+        // System.out.println(sb);
+        // sb.insert(0,'S');        Insertion
+        // sb.insert(2,'e');           
+        // System.out.println(sb);
+        // sb.delete(2,3);
+        // System.out.println(sb);
+        // sb.delete(2,4);          Deletion
+        // System.out.println(sb);
+        // sb.append("Aloo");       Addtion
+        // System.out.println(sb);
+
     }
 }
