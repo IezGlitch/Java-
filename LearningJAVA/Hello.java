5010ff15deccccaf874234faa584af7f45ab7fb0
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-07-06 23:41:30 +0530

    updation

--- LearningJAVA/Hello.java
+++ LearningJAVA/Hello.java
@@ -2,8 +2,8 @@ import java.util.*;
 class Hello{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int a = 66;
-        int b = 40;
+        int a = 68;
+        int b = 44;
         String x = "Gandu";
         x = "Happy Gandu";
         b = 55;
