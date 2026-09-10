8bf699dc41da27e21c9acacab69da04ed07dc8f2
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-09-10 17:09:11 +0530

    updation

--- LearningJAVA/normal.java
+++ LearningJAVA/normal.java
@@ -1,19 +1,48 @@
+// import java.util.*;
+// public class normal {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int temp = n , reverse = 0;
+//         while( temp != 0){
+//             reverse = reverse * 10 + temp % 10;
+//             temp = temp /10;
+//         }
+//         if ( n == reverse){
+//             System.out.println(n + "It is a palindrome number");
+//          }
+//          else
+//             System.out.println("Not a palindrome number");
+         
+//     }
+    
+// }
+//Print First and Last Digit
+
+// import java.util.*;
+// public class normal{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int LastDigit = n%10;
+//         int FirstDigit = n;
+//         while(FirstDigit>=10){
+//             FirstDigit =FirstDigit/10;
+//         }
+//         System.out.println(FirstDigit);
+//         System.out.println(LastDigit);
+//     }
+// }
 import java.util.*;
-public class normal {
+public class normal{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int temp = n , reverse = 0;
-        while( temp != 0){
-            reverse = reverse * 10 + temp % 10;
-            temp = temp /10;
+        int num = sc.nextInt();
+        int FirstDigit = num;
+        int LastDigit = num % 10;
+        while (FirstDigit >= 10){
+            FirstDigit/=10;
         }
-        if ( n == reverse){
-            System.out.println(n + "It is a palindrome number");
-         }
-         else
-            System.out.println("Not a palindrome number");
-         
+        System.out.println("Sum of FirstDigit and LastDigit :" + (FirstDigit+LastDigit));
     }
-    
 }
