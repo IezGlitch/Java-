a1deb21d65791d533e435868574664f321c49a8a
xdiezsus <sharma.shreyansh82929@gmail.com>
2026-09-22 21:39:43 +0530

    updation

--- LearningJAVA/bitManipul.java
+++ LearningJAVA/bitManipul.java
@@ -11,15 +11,49 @@
 //         }
 //     }
 // }
-//SetBit
 
-public class bitManipul {
+//SetBit
+// public class bitManipul {
+//     public static void main(String args[]){
+//         int n = 5;
+//         int pos = 1;
+//         int bitMask = 1<<pos;
+//         int newNum = bitMask | n;
+//         System.out.println(newNum);
+
+//         }
+//     }
+ 
+//Clear Bit
+// public class bitManipul{
+//     public static void main(String args[]){
+//         int no = 5;
+//         int pos = 2;
+//         int bitMask = 1<<2;
+//         int newNum = ~bitMask & no;
+//         System.out.println(newNum);
+//     }
+// }
+
+//Update Bit
+import java.util.*;
+public class bitManipul{
     public static void main(String args[]){
-        int n = 5;
+        Scanner sc = new Scanner(System.in);
+        int num = 5;
         int pos = 1;
-        int bitMask = 1<<pos;
-        int newNum = bitMask | n;
-        System.out.println(newNum);
+        System.out.println("Press what manipulation you want to do");
+        int manipulation = sc.nextInt();
+        if (manipulation == 0){ //Clear Operation
+            int bitMask = 1<<pos;
+            int newNum = ~bitMask & num;
+            System.out.println(newNum);
+        }
+        else{//Set operation
+            int bitMask = 1<<pos;
+            int newNum = bitMask | num;
+            System.out.println(newNum);
 
         }
     }
+}
